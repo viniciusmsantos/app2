334928b23e9600ce24f19efc4fb7139665548d9a
viniciusmsantos <viniciusmarsan@gmail.com>
2019-02-20 15:10:39 -0300

    First commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import{ OfertasService } from '../ofertas.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+  providers: [ OfertasService ]
+})
+export class HomeComponent implements OnInit {
+
+  constructor(private ofertasService: OfertasService) { }
+
+  ngOnInit() {
+    this.ofertasService.getOfertas
+  }
+
+}
