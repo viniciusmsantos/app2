13b1277bc7632a840bac47a7332c4e2706e5749e
viniciusmsantos <viniciusmarsan@gmail.com>
2019-03-29 16:06:47 -0300

    implenta o página diversao

--- src/app/ofertas.service.ts
+++ src/app/ofertas.service.ts
@@ -7,19 +7,29 @@ import 'rxjs'
 @Injectable()
 export class OfertasService {
 
+    private url_api = 'http://localhost:3000/ofertas'
+
     constructor(private http: Http){}
  
     public getOfertas(): Promise<Oferta[]> {
         //efetua requisição http
-        return this.http.get('http://localhost:3000/ofertas?destaque=true')
+        return this.http.get(`${this.url_api}?destaque=true`)
             .toPromise()
             .then((resposta: any) => resposta.json())
         //retorna promise de Oferta[]
     }
 
     public getOfertasPorCategoria(catergoria: string) : Promise<Oferta[]>{
-        return this.http.get(`http://localhost:3000/ofertas?categoria=${catergoria}`)
+        return this.http.get(`${this.url_api}?categoria=${catergoria}`)
             .toPromise()
             .then((resposta: any) => resposta.json())
+    }
+
+    public getOfertaPorId(id: number): Promise<Oferta> {
+        return this.http.get(`${this.url_api}?id=${id}`)
+            .toPromise()
+            .then((resposta: any) => {
+                return resposta.json()[0]
+            })
     }
 }
